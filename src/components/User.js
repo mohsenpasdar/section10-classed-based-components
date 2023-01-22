2cdeb72d3dcb5109aff0e159c38b37715d826d88
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-01-22 21:38:16 +0330

    168

--- src/components/User.js
+++ src/components/User.js
@@ -3,12 +3,21 @@ import { Component } from 'react';
 import classes from './User.module.css';
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log('unmount');
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
 }
 
 // const User = (props) => {
+//   useEffect(() => {
+//     return () => {
+//       console.log('unmount');
+//     }
+//   }, [props.name])
 //   return <li className={classes.user}>{props.name}</li>;
 // };
 
